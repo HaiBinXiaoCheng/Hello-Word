f2e43401fa87824d32bf19993b31dec410871271
海滨小城 <pengbin0708@163.com>
2016-06-01 09:48:20 +0800

    第一次添加

---
+++ HBSegmentView/HBSegmentView.swift
@@ -0,0 +1,21 @@
+//
+//  HBSegmentView.swift
+//  HBSegmentView
+//
+//  Created by 海滨小城 on 16/5/27.
+//  Copyright © 2016年 HBXC. All rights reserved.
+//
+
+import UIKit
+
+class HBSegmentView: UIScrollView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ HBSegmentViewTests/HBSegmentViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  HBSegmentViewTests.swift
+//  HBSegmentViewTests
+//
+//  Created by 海滨小城 on 16/5/27.
+//  Copyright © 2016年 HBXC. All rights reserved.
+//
+
+import XCTest
+@testable import HBSegmentView
+
+class HBSegmentViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
